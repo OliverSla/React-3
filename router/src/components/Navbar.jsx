6375bdf0c01c81b31186b2adba58b1041d6f049c
Peester <oliversladkovsky@gmail.com>
2023-11-07 21:21:10 +0100

    React 3 - NavLink + isActive

--- router/src/components/Navbar.jsx
+++ router/src/components/Navbar.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import {Link, Outlet} from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <nav>
         <ul>
-            <Link to="/" > Domov </Link>
-            <Link to="/movies" > Filmy </Link>
-            <Link to="/serials" > Domov </Link>
+            <NavLink to="/" className={ ({isActive}) => 
+            isActive ? "active__link link" : "non__active__link" } > Domov </NavLink>
+            <NavLink to="/movies" > Filmy </NavLink>
+            <NavLink to="/serials" > Domov </NavLink>
         </ul>
     </nav>
   )
