4a88901938154a656a36b641d497cb53a3dcdfea
Peester <oliversladkovsky@gmail.com>
2023-11-11 23:28:28 +0100

    React 3 - načítanie a vypísanie dat z databázy + catch error

--- firebase/firebase-course/src/App.jsx
+++ firebase/firebase-course/src/App.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import { db } from "./firebase/config";
+import { useEffect, useState } from "react";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(false);
 
-export default App
+  useEffect(() => {
+    db.collection("movies")
+      .get()
+      .then((snapshot) => {
+        console.log(snapshot);
+        if (snapshot.empty) {
+          setError("Žiadne filmy");
+        } else {
+          let result = [];
+          snapshot.docs.forEach((oneMovie) => {
+            result.push({ id: oneMovie.id, ...oneMovie.data() });
+          });
+          console.log(result);
+          setData(result);
+        }
+      })
+      .catch((err) => {
+        setError(err.massage);
+      });
+  }, []);
+
+  return <div>
+    {error && <p>{error}</p>}
+    {
+      data.map((oneMovie) => {
+        return(
+          <div key={oneMovie.id}>
+            <h1>{oneMovie.title}</h1>
+            <h3>{oneMovie.time} min.</h3>
+            <h5>{oneMovie.minage}+</h5>
+           </div>
+        )
+      })
+    }
+  </div>;
+};
+
+export default App;
