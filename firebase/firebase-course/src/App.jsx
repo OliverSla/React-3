a06187698133fef617a86410dd6477540e7c25f2
Peester <oliversladkovsky@gmail.com>
2023-11-12 02:13:43 +0100

    React 3 - vymazanie filmu z db a následne vykreslenie filmov pomocou useState

--- firebase/firebase-course/src/App.jsx
+++ firebase/firebase-course/src/App.jsx
@@ -26,20 +26,29 @@ const App = () => {
       });
   }, []);
 
-  return <div>
-    {error && <p>{error}</p>}
-    {
-      data.map((oneMovie) => {
-        return(
-          <div key={oneMovie.id}>
+
+const deleteMovie = (id) => {
+   db.collection("movies").doc(id).delete(); 
+  const newData = data.filter((oneMovie) => oneMovie.id !== id)
+  setData(newData)
+}
+
+  return (
+    <div className="all_movies_wrapper">
+      {error && <p>{error}</p>}
+      {data.map((oneMovie, index) => {
+        return (
+          <div className="movie_wrapper" key={oneMovie.id}>
+            <h6>{index + 1}. film v poradí</h6>
+            <h5>{oneMovie.minage}+</h5>
             <h1>{oneMovie.title}</h1>
             <h3>{oneMovie.time} min.</h3>
-            <h5>{oneMovie.minage}+</h5>
-           </div>
-        )
-      })
-    }
-  </div>;
+            <button onClick={() => deleteMovie(oneMovie.id)}>Zmazať film</button>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default App;
