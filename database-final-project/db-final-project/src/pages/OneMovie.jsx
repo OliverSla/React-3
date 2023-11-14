cb3637439f015e376e6f19b7ebf0a78765eff153
Peester <oliversladkovsky@gmail.com>
2023-11-14 21:59:59 +0100

    React 3 Final Project - prelink ku každému filmu v poli, link v URL podľa ID, zobrazenie jedného filmu na jednej podstránke

--- database-final-project/db-final-project/src/pages/OneMovie.jsx
+++ database-final-project/db-final-project/src/pages/OneMovie.jsx
@@ -1,15 +1,40 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React from "react";
+import { useParams } from "react-router-dom";
+import { db } from "../firebase/config";
+import { useState, useEffect } from "react";
 
 const OneMovie = () => {
+  const [data, setData] = useState({});
+  const [error, setError] = useState("");
+  const { movieID } = useParams();
 
-  const {movieID} = useParams()
-
-  console.log(movieID)
+  useEffect(() => {
+    db.collection("movies")
+      .doc(movieID)
+      .get()
+      .then((myDoc) => {
+        if (myDoc.exists) {
+          setData(myDoc.data());
+        } else {
+          setError("Nenašiel sa film");
+        }
+      })
+      .catch((err) => {
+        setError("Nastala chyba: " + err.message);
+      });
+  }, []);
 
   return (
-    <div>{movieID}</div>
-  )
-}
+    <div className="oneMovie_wrapper">
+      {error && <p>{error}</p>}
+      <div className="oneMovie">
+        <h1>{data.title}</h1>
+        <p>{data.text}</p>
+        <h4>Minimálny vek: {data.minage}</h4>
+        <h5>Čas: {data.time}</h5>
+      </div>
+    </div>
+  );
+};
 
-export default OneMovie
+export default OneMovie;
